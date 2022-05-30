52596a701cc54115d2b3219904bd493437c63984
xqm32 <458173774@qq.com>
2022-05-30 23:08:23 +0800

    add csv_default

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ csv_value_not_null = "不为空"
 csv_value_unique = "唯一"
 
 csv_chinese_name = "对应中文属性名"
+csv_default = "默认值"
 
 primary_key = ""
+
 
 def convert(data_type):
     data_type = re.findall(r"[a-zA-Z]+", data_type)[0].upper()
@@ -37,9 +39,12 @@ def convert(data_type):
 
 def resolve_row(row):
     global primary_key
+
     data_type = " " + convert(row[csv_data_type])
-    not_null = " NOT NULL" if type(row[csv_constraint]) is str and csv_value_not_null in row[csv_constraint] else ""
-    unique = " UNIQUE" if type(row[csv_constraint]) is str and csv_value_unique in row[csv_constraint] else ""
+    not_null = " NOT NULL" if csv_value_not_null in row[csv_constraint] else ""
+    unique = " UNIQUE" if csv_value_unique in row[csv_constraint] else ""
+    default_value = " DEFAULT " + row[csv_default] if row[csv_default] else ""
+
     if row[csv_is_key] == csv_value_primary:
         autoincrement = ""
         if "ID" in row[csv_column_name].upper():
@@ -47,16 +52,26 @@ def resolve_row(row):
             autoincrement = " AUTOINCREMENT"
 
         primary_key = f", PRIMARY KEY ({row[csv_chinese_name]}{autoincrement})"
-    return f"{row[csv_chinese_name]}{data_type}{unique}{not_null}"
+
+    return f"{row[csv_chinese_name]}{data_type}{unique}{not_null}{default_value}"
+
 
 def resolve_csv(csv_file):
     print("Resolving csv file: " + csv_file)
+
     global primary_key
     primary_key = ""
+
     table_name = csv_file.rstrip(".csv")
+
     schema = pandas.read_csv(csv_file, encoding="utf-8")
+    schema.fillna("", inplace=True)
+    if csv_default not in schema.columns:
+        schema[csv_default] = ""
+
     table_schema = ", ".join(resolve_row(i[1]) for i in schema.iterrows()) + primary_key
     return table_template.format(table_name=table_name, table_schema=table_schema)
+
 
 files = filter(lambda i: i.endswith(".csv"), os.listdir())
 with open("hrms.sql", "w", encoding="utf-8_sig") as f:
